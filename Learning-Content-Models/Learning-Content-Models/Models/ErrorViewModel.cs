317a48d1b2542bc38f8185d8289ec833baea403b
pavkata2503 <pavkatapetrov@gmail.com>
2023-12-09 17:15:17 +0200

    Delete

--- Learning-Content-Models/Learning-Content-Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Learning_Content_Models.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
